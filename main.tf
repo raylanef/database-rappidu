2adf249d38635b85e0ce57c310c5636aa8016cd1
raylanef <raylanefernandes@hotmail.com>
2024-10-03 19:19:50 -0300

    acesso publico

--- main.tf
+++ main.tf
@@ -21,6 +21,26 @@ variable "db_name" {
   default     = "rappidu"
 }
 
+resource "aws_security_group" "rds_public_access" {
+  name        = "rds_public_access_sg"
+  description = "Security Group para acesso público ao RDS MySQL"
+  vpc_id      = aws_vpc.main.id  # Certifique-se de que a VPC exista ou defina aqui uma.
+
+  ingress {
+    from_port   = 3306              # Porta do MySQL
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]     # Permite acesso de qualquer IP público
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"              # Permite todo o tráfego de saída
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "rappidu_db" {
   allocated_storage    = 20      
   engine               = "mysql"
@@ -31,6 +51,7 @@ resource "aws_db_instance" "rappidu_db" {
   parameter_group_name = "default.mysql8.0"
   publicly_accessible  = true
   skip_final_snapshot  = true
+  vpc_security_group_ids = [aws_security_group.rds_public_access.id] 
   storage_type         = "gp2"           
   engine_version       = "8.0"
   backup_retention_period = 7             
